39ab51c77b63c04c737f086e6bc678c1ceab3dfb
softlang-net <x915927@icloud.com>
2023-06-03 11:36:29 +0800

    hello

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
-	"softlang.net/podsdog/dog"
 	"softlang.net/podsdog/models"
 )
 
@@ -11,6 +13,43 @@ func main() {
 	item := new(models.PodItem)
 	item.Name = "hello"
 	fmt.Println(item)
-	dog.EnsureService(item)
+	// dog.EnsureService(item)
 	fmt.Printf("%+v\n", item)
+	testEnv()
+}
+
+func testEnv() {
+	// Open the file
+	file, err := os.Open("conf/env.cfg")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Create a scanner
+	scanner := bufio.NewScanner(file)
+
+	// Read each line
+	for scanner.Scan() {
+		// Print the line
+		line := strings.TrimSpace(scanner.Text())
+
+		fmt.Println(line)
+		if len(line) > 0 && line[0] != '#' {
+			ll := strings.SplitN(line, "=", 2)
+			if len(ll) == 2 {
+				v1 := strings.Trim(strings.Trim(ll[1], "'"), "\"")
+				os.Setenv(ll[0], v1)
+			}
+		}
+	}
+	fmt.Println(">> env ----")
+
+	for _, str := range os.Environ() {
+		fmt.Println(str)
+	}
+
+	//if line.startswith('#') or not line.strip():
+	//os.Setenv("hell")
 }
--- models/pod_entity.go
+++ models/pod_entity.go
@@ -1,9 +1,9 @@
 package models
 
 type PodItem struct {
-	Name     string `json:"service name"`
-	Image    string `json:"Image name"`
-	Tag      string `json:"Image tag"`
-	Network  string `json:"network name"`
-	LabelDir string `json:"vritual path"`
+	Name     string `json:"service_name"`
+	Image    string `json:"Image_name"`
+	Tag      string `json:"Image_tag"`
+	Network  string `json:"network_name"`
+	LabelDir string `json:"vritual_path"`
 }
